c22c121e19edfa8e1b2fabb24c8d0a432a560af0
Andre Pires <andre.pires@xpand-it.com>
2019-08-27 23:49:31 +0100

    Commit

--- release.sh
+++ release.sh
@@ -17,9 +17,18 @@ NEXT_DEVELOPMENT_VERSION=$2
 
 echo "Releasing: '$RELEASE_VERSION' -> Next Dev Version: '$NEXT_DEVELOPMENT_VERSION'"
 
+echo "git checkout master"
+git checkout master
+
 # 1st - Maven release prepare dry run (used to maven and git basic validations before startint the release process)
 echo "mvn -B release:prepare -D releaseVersion=$RELEASE_VERSION -D developmentVersion=$NEXT_DEVELOPMENT_VERSION -D scmCommentPrefix=$GIT_COMMIT_COMMENT_PREFIX -D dryRun=true"
 mvn -B release:prepare -D releaseVersion="$RELEASE_VERSION" -D developmentVersion="$NEXT_DEVELOPMENT_VERSION" -D scmCommentPrefix="$GIT_COMMIT_COMMENT_PREFIX" -D dryRun=true
+MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE=$?
+if [ "$MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE" -ne 0 ]; then
+  echo "Error '$MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE' when maven was executing a dry run before preparing the release"
+  mvn release:clean
+  exit 2
+fi
 
 # 2nd - Maven release prepare
 echo "mvn -B release:prepare -Dresume=false  -D releaseVersion=$RELEASE_VERSION -D developmentVersion=$NEXT_DEVELOPMENT_VERSION -D scmCommentPrefix=$GIT_COMMIT_COMMENT_PREFIX"
@@ -28,6 +37,7 @@ mvn -B release:prepare -Dresume=false -D releaseVersion="$RELEASE_VERSION" -D de
 MVN_RELEASE_PREPARE_EXIT_CODE=$?
 if [ "$MVN_RELEASE_PREPARE_EXIT_CODE" -ne 0 ]; then
   echo "Error '$MVN_RELEASE_PREPARE_EXIT_CODE' when maven was preparing the release"
+  mvn release:clean
   exit 2
 fi
 
