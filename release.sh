41744a9ae55776800316fd949d4fed91e92714b8
Andre Pires <andre.pires@xpand-it.com>
2019-08-27 23:53:13 +0100

    Commit

--- release.sh
+++ release.sh
@@ -1,6 +1,15 @@
+#!/bin/sh
 ###########################################################################################################
 #   Release.sh is a wrapper script around maven release plugin and git to perform a fully release flow    #
 ###########################################################################################################
+
+# call this function to write to stderr
+echo_stderr ()
+{
+    echo "$@" >&2
+}
+
+# ----------------------------------------------------------------------------------------------------------
 
 # Release script pre-Validations for a smooth release process...
 if [ "$#" -ne 2 ]; then
@@ -19,15 +28,20 @@ echo "Releasing: '$RELEASE_VERSION' -> Next Dev Version: '$NEXT_DEVELOPMENT_VERS
 
 echo "git checkout master"
 git checkout master
+GIT_CHECKOUT_EXIT_CODE=$?
+if [ "$GIT_CHECKOUT_EXIT_CODE" -ne 0 ]; then
+  echo_stderr "Error '$GIT_CHECKOUT_EXIT_CODE' checking out the git's master branch"
+  exit 2
+fi
 
 # 1st - Maven release prepare dry run (used to maven and git basic validations before startint the release process)
 echo "mvn -B release:prepare -D releaseVersion=$RELEASE_VERSION -D developmentVersion=$NEXT_DEVELOPMENT_VERSION -D scmCommentPrefix=$GIT_COMMIT_COMMENT_PREFIX -D dryRun=true"
 mvn -B release:prepare -D releaseVersion="$RELEASE_VERSION" -D developmentVersion="$NEXT_DEVELOPMENT_VERSION" -D scmCommentPrefix="$GIT_COMMIT_COMMENT_PREFIX" -D dryRun=true
 MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE=$?
 if [ "$MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE" -ne 0 ]; then
-  echo "Error '$MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE' when maven was executing a dry run before preparing the release"
+  echo_stderr "Error '$MVN_RELEASE_PREPARE_DRY_RUN_EXIT_CODE' when maven was executing a dry run before preparing the release"
   mvn release:clean
-  exit 2
+  exit 3
 fi
 
 # 2nd - Maven release prepare
@@ -36,9 +50,9 @@ mvn -B release:prepare -Dresume=false -D releaseVersion="$RELEASE_VERSION" -D de
 
 MVN_RELEASE_PREPARE_EXIT_CODE=$?
 if [ "$MVN_RELEASE_PREPARE_EXIT_CODE" -ne 0 ]; then
-  echo "Error '$MVN_RELEASE_PREPARE_EXIT_CODE' when maven was preparing the release"
+  echo_stderr "Error '$MVN_RELEASE_PREPARE_EXIT_CODE' when maven was preparing the release"
   mvn release:clean
-  exit 2
+  exit 4
 fi
 
 # 3rd - Maven release perform
@@ -47,8 +61,8 @@ mvn -B release:perform
 
 MVN_RELEASE_PERFORM_EXIT_CODE=$?
 if [ "$MVN_RELEASE_PERFORM_EXIT_CODE" -ne 0 ]; then
-  echo "Error '$MVN_RELEASE_PERFORM_EXIT_CODE' when maven was performing the release"
-  exit 3
+  echo_stderr "Error '$MVN_RELEASE_PERFORM_EXIT_CODE' when maven was performing the release"
+  exit 5
 fi
 
 # 4th - Git merge to master branch
